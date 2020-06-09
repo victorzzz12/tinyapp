e27b2b74c22c4a45e3d25a6d5acc41ffb6b7d1b1
Victor Liang <victor.zw.liang@gmail.com>
2020-06-09 20:52:52 +0000

    Added login and logout features

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
 
 
 function generateRandomString() {
@@ -13,6 +14,7 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,18 +22,19 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  let templateVars = { shortURL: shortURL, longURL: longURL };
+  let templateVars = { shortURL: shortURL, longURL: longURL, username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -60,9 +63,21 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/urls/:shortURL/edit", (req, res) => {
+app.post("/urls/:shortURL/edit", (req, res) => { //goes into the short url from the edit button
   res.redirect(`/urls/${req.params.shortURL}`);
 });
+
+app.post("/login", (req, res) => { //logins with given username
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+  let templateVars = { username: req.cookies["username"] }
+  res.render("urls_index", templateVars);
+});
+
+app.post("/logout", (req, res) => { //logs out current user
+  (res.clearCookie('username', req.body.username));
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
