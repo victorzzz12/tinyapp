a2874155e2add0625f4425435aed80ec17d267de
Victor Liang <victor.zw.liang@gmail.com>
2020-06-11 21:49:11 +0000

    Added tests for findUserByEmail function

---
+++ test/helpersTest.js
@@ -0,0 +1,19 @@
+const { assert } = require('chai');
+const { users } = require("../userDatabase");
+const { findUserByEmail } = require("../helperFunctions");
+
+describe('findUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", users)
+    const expectedOutput = "userRandomID";
+    
+    assert.equal(user, expectedOutput);
+  });
+
+  it('should return false with invalid email', function() {
+    const user = findUserByEmail("hahaha@hotmail.com", users)
+    const expectedOutput = false;
+    
+    assert.equal(user, expectedOutput);
+  });
+});
